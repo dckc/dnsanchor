0f9e820338386b3046b664ff59e395f28a1eff0c
Dan Connolly <dckc@madmode.com>
2019-11-29 19:06:28 -0600

    move testAuth out of index.js

--- index.js
+++ index.js
@@ -8,32 +8,24 @@ import asPromise from './lib/aspromise';
 const harden = x => Object.freeze(x);
 
 
-function testAuth(crypto, sha1hex) {
-  const example = 'testuser;1012121212;dkwo28Sile4jdXkw;p3kxmRKf9dk3l6ls;/site/example/getInfo;da39a3ee5e6b4b0d3255bfef95601890afd80709';
-  const expected = 'testuser;1012121212;dkwo28Sile4jdXkw;0fa8932e122d56e2f6d1550f9aab39c4aef8bfc4';
-  const [login, timestamp, salt, apiKey, url, bodyHash] = example.split(';');
-  const actual = auth(url, '', { login, apiKey }, { timestamp, salt }, sha1hex);
-  console.log({actual, expected, OK: actual === expected});
-}
-
 async function main({ fsp, path, https, crypto, clock }) {
   const sha1hex = txt => crypto.createHash('sha1').update(txt).digest('hex');
   const randomBytesHex = qty => crypto.randomBytes(qty).toString('hex');
 
-  // testAuth(crypto, sha1hex);
+  // auth.test(crypto, sha1hex);
 
-  const cwd = makeNodePath('.', { fsp, path });
   const web = harden({
     https: (host, port) => makeNodeHttpPath(`https://${host}:${port}/`, {},
 					    { request: https.request, resolve: url.resolve }),
   });
-  const config = await readData(cwd.join('config.json'));
-
   const ip = await web.https(ipAddressService, 443).readFile();
 
+  const cwd = makeNodePath('.', { fsp, path });
+  const config = await readData(cwd.join('config.json'));
   const domain = nfsnEndPoint(config.login, config.API_KEY,
 			      { web, clock, randomBytesHex, sha1hex })
 	.domain(config.domain);
+
   await ensureCurrent(ip, domain, config.host);
 }
 
